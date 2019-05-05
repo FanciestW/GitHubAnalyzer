9c989d20c19663f1accf87ec9636914aa6f6b5e6
William Lin <wlin26@yahoo.com>
2019-05-05 10:15:58 -0400

    Added analysis for top location and languages based on location

--- GitHubAnalyzer/Analyzer.py
+++ GitHubAnalyzer/Analyzer.py
@@ -5,7 +5,15 @@ class Analyzer:
     filename: str                   # The name of the input file for data.
     data: pd.DataFrame              # Data as pandas dataframes.
 
-    def __init__(self, file: str):
+    def __init__(self, file):
+        """
+            Create an Analyzer object and sets the dataframes to input file data.
+
+            Parameters
+            ----------
+            file: str
+                Path to input data file. Either a JSON or CSV file.
+        """
         self.filename = file
         if file.endswith('.csv'):
             self.data = pd.read_csv(self.filename)
@@ -15,9 +23,9 @@ class Analyzer:
             print('File must be a JSON or CSV file.')
             sys.exit(0)
 
-    def getTopLanguages(self, num: int) -> list:
+    def topLanguages(self, num):
         """
-            Gets the top repository languages in descending order.
+            Returns the top repository languages in descending order.
             
             Parameters
             ----------
@@ -26,7 +34,7 @@ class Analyzer:
             
             Returns
             -------
-            list: str
+            list: tuple
                 A list of tuples containing the top languages and their
                 popularity based on how many repositories used that language.
         """
@@ -36,23 +44,67 @@ class Analyzer:
         top_langs = lang_rank['repository_url']
         return (top_langs.index.values, top_langs.values)
 
-    def getTopCountries(self, num: int) -> list:
+    def topActorLocations(self, num):
         """
-            Gets the top countries for repository creation in descending order.
+            Returns the top locations for repository contribution in descending order.
 
             Parameters
             ----------
             num: int
-                The number of top countries to return.
+                The number of top locations to return.
 
             Returns
             -------
-            list: str
-                A list of strings containing the top countries.
+            list: tuple
+                A list of tuples containing the top locations and their contribution
+                counts based on how many actors contributed to the repositories.
+        """
+        country_count = self.data.groupby('actor_attributes_location').count()
+        country_rank = country_count.sort_values('repository_url', ascending=False)
+        country_rank = country_count.nlargest(num, ['repository_url'], keep='all')
+        top_actor_countries = country_rank['repository_url']
+        return (top_actor_countries.index.values, top_actor_countries.values)
+
+    def topRepoLocations(self, num):
+        """
+            Returns the top locations for creating repositories.
+
+            Parameters
+            ----------
+            num: int
+                The number of top locations to return.
+
+            Returns
+            -------
+            list: tuple
+                A list of tuples containing the top locations based on repository count.
         """
         pass
 
-    def getPopularRepo(self, num: int) -> list:
+    def countryTopLanguages(self, country, num):
+        """
+            Returns a given countries top list of programming languages.
+
+            Parameters
+            ----------
+            country: str
+                The country name to get top languages on.
+            num: int
+                The number of top languages to return.
+
+            Returns
+            -------
+            list: tuple
+                A list of tuples containing the top languages and their repository count.
+        """
+        country_data = self.data.loc[self.data['actor_attributes_location'] == country]
+        count = country_data.groupby('repository_language').count()
+        lang_rank = count.sort_values('repository_url', ascending=False)
+        lang_rank = lang_rank.nlargest(num, ['repository_url'], keep='all')
+        top_country_languages = lang_rank['repository_url']
+        return (top_country_languages.index.values, top_country_languages.values)
+
+    def getPopularRepo(self, num: int):
         """
             Gets the top most popular repositories.
 
--- main.py
+++ main.py
@@ -17,9 +17,15 @@ def main():
     analyzer = Analyzer(args.file)
     grapher = Grapher()
 
-    # Get top 10 languages and graph on bar graph.
-    top_10_langs = analyzer.getTopLanguages(10)
-    grapher.top_bar_chart(top_10_langs[0], top_10_langs[1], "Repo Count", "Top 10 Languages")
+    # # Get top 10 languages and graph on bar graph.
+    # top_10_langs = analyzer.topLanguages(10)
+    # grapher.top_bar_chart(top_10_langs[0], top_10_langs[1], 'Repo Count', 'Top 10 Languages')
 
+    # Get top 10 countries for repository creations.
+    top_actor_countries = analyzer.topActorLocations(10)
+    grapher.top_bar_chart(top_actor_countries[0], top_actor_countries[1], 'Actor Count', 'Top 10 Actor Countries')
+
+    # Get a Countries top programming languages.
+    print(analyzer.countryTopLanguages('San Francisco, CA', 10))
 if __name__ == "__main__":
     main()
